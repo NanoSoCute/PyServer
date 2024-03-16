4f843ef72d8c70bbad984d41805ae5bb3b94328e
Rin <150316145+NanoSoCute@users.noreply.github.com>
2024-03-16 05:15:05 -0700

    Update mainserver.py

--- mainserver.py
+++ mainserver.py
@@ -3,11 +3,9 @@ import subprocess
 
 def open_program(program_number):
     if program_number == "1":
-        program_path = "path/to/your/program1.exe"  # Replace with the actual path
-        subprocess.Popen(program_path)
+        print('Hello World!')
     elif program_number == "2":
-        program_path = "path/to/your/program2.exe"  # Replace with the actual path
-        subprocess.Popen(program_path)
+        print('Hello World!')
     else:
         print("Invalid input. Please enter 1 or 2.")
 
