0c838fc7411ca00a54548a21d865a9159f2121e5
Rin <150316145+NanoSoCute@users.noreply.github.com>
2024-03-16 05:33:08 -0700

    Update mainserver.py

--- mainserver.py
+++ mainserver.py
@@ -15,9 +15,8 @@ def read_credentials_from_github(repo_url, filename):
         raise Exception("Failed to fetch file from GitHub")
 
 def login():
-    repo_url = 'https://raw.githubusercontent.com/NanoSoCute/PyServer/main/assets/credentials'
-    filename = 'logincredentials.txt'
-    credentials = read_credentials_from_github(repo_url, filename)
+    repo_url = 'https://raw.githubusercontent.com/NanoSoCute/PyServer/main/assets/credentials/logincredentials.txt'
+    credentials = read_credentials_from_github(repo_url)
     
     while True:
         username = input("Enter your username: ")
